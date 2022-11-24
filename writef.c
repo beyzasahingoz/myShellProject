dfb77ab33f364171f4f3ca9a60c84484c08b0c8e
beyza <70069234+beyzasahingoz@users.noreply.github.com>
2022-11-24 18:17:22 +0300

    Update writef.c

--- writef.c
+++ writef.c
@@ -5,27 +5,29 @@
 
 int main(int argc, char *argv[])
 {
+   //defined variables
    FILE *fptr;
    char c;
    size_t size = 10;
    int p_id, p_pid;
    time_t seconds;
    struct tm *timeStruct;
-
+   //received current time.
    time(&seconds);
    timeStruct = localtime(&seconds);
-
+   //received pid and ppid
    p_id = getpid();
    p_pid = getppid();
-
+   //received input from user.
    char *input = malloc(sizeof(char) * size);
    printf("Enter input: ");
    getline(&input, &size, stdin);
-
+   //if the file is not created, created and writed by user.
    fptr = fopen(argv[argc - 1], "a");
-
+   //if the file is created, writed by user.
    if (fptr != NULL)
    {
+      //text and time are printed to the file.
       fprintf(fptr, "%s\n", input);
       fprintf(fptr, "Current time : %d:%d:%d pid: %d ppid: %d\n--\n", timeStruct->tm_hour, timeStruct->tm_min, timeStruct->tm_sec, p_id, p_pid);
       fclose(fptr);
@@ -38,7 +40,7 @@ int main(int argc, char *argv[])
       fclose(fptr);
       return 0;
    }
-
+   //text and time are printed to the file.
    fprintf(fptr, "Current time : %d:%d:%d pid: %d ppid: %d\n--\n", timeStruct->tm_hour, timeStruct->tm_min, timeStruct->tm_sec, p_id, p_pid);
    fprintf(fptr, "%s\n", input);
 
