b6218a3e7f879417219de771381d5bdb9bab856a
beyza <70069234+beyzasahingoz@users.noreply.github.com>
2022-11-29 13:14:28 +0300

    Update myshell.c

--- myshell.c
+++ myshell.c
@@ -12,7 +12,7 @@ while(1){
 	char **input = ((char **) malloc (100*sizeof(char**)));
 	//defined variable and receipt input from user.
 	int m=0, size=0; 
-		printf("myshell>>");
+		printf("\nmyshell>>");
 		gets(userInput,100,stdin); 
 		//the input received was divided according to its spaces with the strtok method.
 		temp =strtok(userInput, " ");
@@ -30,19 +30,28 @@ while(1){
 	free(userInput);
    }
 }
-//operations are performed according to the commands entered by the user.
+//operations are performed according to the commands entered by the user and check the inputs.
 void userCommand(char *input[]){
+	int f,stat;
+	int size= findLength(input);
 	if(strcmp(input[0],"exit")==0){
 		exit(0);
-	}else if(strcmp(input[0],"bash")==0){
+	}else if(strcmp(input[0],"bash")==0 && size==1){
   		system("/bin/bash");
-  	}else if(strcmp(input[0],"cat")==0){
-  		printf("cat:%s\n",input[1]);
-  	}else if(strcmp(input[0],"ls")==0){
+  	}else if(strcmp(input[0],"cat")==0 && size>1){
+		f=fork();
+		if(f==0){
+			stat = execv("/bin/cat",input);
+			perror("exec failed");
+		}else{
+			//wait child process
+			wait(&stat);
+		}
+  	}else if(strcmp(input[0],"ls")==0 && size==1){
     		system("/bin/ls");
-   	}else if (strcmp(input[0],"clear")==0){
+   	}else if (strcmp(input[0],"clear")==0 && size==1){
         	system("clear");
-    }else if(strcmp(input[0],"execx")==0){
+    }else if(strcmp(input[0],"execx")==0 && !strcmp(input[1], "-t")&& isNumber(input[2])== 1 && size>=4){
   		int i,m;
   		i = fork();
             	if(i == 0){  
@@ -51,7 +60,7 @@ void userCommand(char *input[]){
             	}else{
                 	wait(&m);
             	}  	
-  	}else if(strcmp(input[0],"writef")==0){
+  	}else if(strcmp(input[0],"writef")==0 && !strcmp(input[1], "-f")&& input[2] != NULL && size==3){
   		int i,m;
   		i = fork();
             	if(i == 0){
@@ -64,3 +73,24 @@ void userCommand(char *input[]){
   		printf("Wrong command!\n");
   	}
 }
+//this method checks if the input is a number.
+int isNumber(char *n){
+	int i=strlen(n);
+	int isNum=(i>0);
+	while(i-- && isNum){
+		if(!(n[i]>='0'&& n[i]<='9')){
+			isNum=0;
+		}
+	}
+	return isNum;
+}
+// this method finds argv length.
+int findLength(char **input[]){
+	int size=0,i;
+	for (i = 0; input[i]!='\0'; i++)
+	{
+		size++;
+	}
+	return size;
+	
+} 
