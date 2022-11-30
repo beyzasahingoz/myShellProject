a27e90d0374b3c28c26f816b5f9976411a345220
beyza <70069234+beyzasahingoz@users.noreply.github.com>
2022-11-30 15:30:52 +0300

    Update execx.c

--- execx.c
+++ execx.c
@@ -4,35 +4,53 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-int main(int argc, char * argv[]){
-    //the command is executed whichever argv equals that string
-    int f,j;
-    if(strcmp(argv[3], "clear") == 0 && argc==4){
-        printf("\033[H\033[J"); 
-    } else if(strcmp(argv[3], "ls") == 0 && argc==4){
-        for(int i = 0; i < atoi(argv[2]); i++){
+int main(int argc, char *argv[])
+{
+    // the command is executed whichever argv equals that string
+    int f, j;
+    if (strcmp(argv[3], "clear") == 0 && argc == 4)
+    {
+        printf("\033[H\033[J");
+    }
+    else if (strcmp(argv[3], "ls") == 0 && argc == 4)
+    {
+        for (int i = 0; i < atoi(argv[2]); i++)
+        {
             system("/bin/ls");
         }
-    } else if(strcmp(argv[3], "bash") == 0 && argc==4){
-        for(int i = 0; i < atoi(argv[2]); i++){
+    }
+    else if (strcmp(argv[3], "bash") == 0 && argc == 4)
+    {
+        for (int i = 0; i < atoi(argv[2]); i++)
+        {
             system("/bin/bash");
         }
-    } else if(strcmp(argv[3], "exit") == 0){
+    }
+    else if (strcmp(argv[3], "exit") == 0)
+    {
         exit(0);
-    } else if(strcmp(argv[3], "writef") == 0 && !strcmp(argv[4], "-f")&& argv[5] != NULL && argc==6){
-        for(int i = 0; i < atoi(argv[2]); i++){
-            //the fork runs until the number entered
-            f=fork();
-            if(f==0){
-                j= execve("writef", argv,NULL);
+    }
+    else if (strcmp(argv[3], "writef") == 0 && !strcmp(argv[4], "-f") && argv[5] != NULL && argc == 6)
+    {
+        for (int i = 0; i < atoi(argv[2]); i++)
+        {
+            // the fork runs until the number entered
+            f = fork();
+            if (f == 0)
+            {
+                j = execve("writef", argv, NULL);
                 exit(0);
                 perror("exec failed");
-            }else{
+            }
+            else
+            {
                 wait(&j);
             }
         }
-    } else {
-        //if the input is not equal to any command, the input is written
+    }
+    else
+    {
+        // if the input is not equal to any command, the input is written
         printf("Wrong Command!");
     }
 }
